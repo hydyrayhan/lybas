4334d930e9273891b4417dc08c4db804c1a71ab6
hydyrayhan <hydyrowayhan7@gmail.com>
2023-11-15 21:51:24 +0500

    config

--- admin/src/common/Config.js
+++
@@ -1,4 +0,0 @@
-export const api = 'http://192.168.57.6:5010/';
-// export const api = 'http://192.168.1.148:5010/';
-// export const api = 'http://216.250.12.252:5010/';
-// export const api = 'http://192.168.100.14:5010/';
